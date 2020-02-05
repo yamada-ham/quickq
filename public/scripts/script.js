ec81a482f302fbcf2f2d313518ad557bf4182e5d
yamada <z1ametapi@gmail.com>
2020-02-06 00:18:37 +0900

    不要なフォルダファイルを削除修正

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -157,7 +157,7 @@ class Quickq{
       var menu = $('.drwerMenuBox');
     	var btn = $('#menubtn');
 
-    		$('.overlay').prepend('<div class="btnCloseBox"><img src="./image/icon/btnClose.png"></div>');
+    		$('.overlay').prepend('<div class="btnCloseBox"><img src="image/icon/btnClose.png"></div>');
 
     	btn.on('click',function(){
     		btn.addClass('drawer');
